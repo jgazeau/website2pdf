a6b049de6aea7fe1f84db6636d269ae7fe16b5d6
Jordan GAZEAU <jordan.gazeau@gmail.com>
2022-05-21 10:21:58 +0200

    fix: ICliArguments

--- src/cli/iArgumentsParser.ts
+++ src/cli/iArgumentsParser.ts
@@ -1,11 +1,11 @@
-import yargs = require('yargs');
 import {PathLike} from 'fs-extra';
+import {Arguments, Argv} from 'yargs';
 
-export interface IArgumentsParser extends yargs.Argv {
+export interface IArgumentsParser extends Argv {
   argv: ICliArguments;
 }
 
-export interface ICliArguments extends yargs.Arguments {
+export interface ICliArguments extends Arguments {
   debug: boolean;
   displayHeaderFooter: boolean;
   sitemapUrl: string;
